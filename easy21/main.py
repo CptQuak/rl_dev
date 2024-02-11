cf53280e4d365e8610a1cb4573ded89e47fb8e15
quak_linux <kamilkulesza5@gmail.com>
2024-02-11 21:44:28 +0100

    Update easy21 game code

--- easy21/main.py
+++ easy21/main.py
@@ -34,68 +34,68 @@ class Easy21:
         self.colors = ["B", "R"]
         self.colors_prob = [2 / 3, 1 / 3]
         self.actions = ["HIT", "STICK"]
-        self.player_cards, self.dealer_cards = [], []
-        self.isterminal = False
-
-    def reset(self):
-        """Reset environment"""
-        self.player_cards, self.dealer_cards = [], []
-        self.isterminal = False
+        self.player_cards, self.dealer_cards = None, None
+        self.isterminal = None
+        self.dealer_card = None
 
     def calc_score(self, cards):
         """Calculate score based on card"""
         return sum(value if color == "B" else -value for value, color in cards)
 
-    def is_not_terminal(self, score):
-        """Evaluates hand of cards and return whether state is terminal"""
-        return 1 < score < 21
-
-    def start_episode(self):
+    def restart_episode(self):
         """Initial draw of cards"""
         self.player_cards = [(np.random.choice(self.cards), "B")]
         self.dealer_cards = [(np.random.choice(self.cards), "B")]
+        self.isterminal = False
+        self.dealer_card = self.dealer_cards[0][0]
 
     def step(self, action):
         """
         State = (player score, dealear first card, reward, terminal state)
         """
-        dealer_card = self.dealer_cards[0][0]
         if self.isterminal:
             return False
 
         if action == "HIT":
-            card, color = np.random.choice(self.cards), np.random.choice(self.colors, p=self.colors_prob)
-            self.player_cards.append((card, color))
-            score = self.calc_score(self.player_cards)
-            # if player lost then negative reward
-            state = (score, dealer_card, 0, False) if self.is_not_terminal(score) else (score, dealer_card, -1, True)
+            state = self._step_player()
 
         if action == "STICK" or self.calc_score(self.player_cards) == 21:
-            # dealer card drawing rule
-            while 0 < self.calc_score(self.dealer_cards) < 17:
-                card, color = np.random.choice(self.cards), np.random.choice(self.colors, p=self.colors_prob)
-                self.dealer_cards.append((card, color))
+            state = self._step_dealer()
 
-            player_score, dealer_score = self.calc_score(self.player_cards), self.calc_score(self.dealer_cards)
-            if dealer_score > 21 or player_score > dealer_score:
-                state = (player_score, dealer_card, 1, True)
-            elif player_score == dealer_score:
-                state = (player_score, dealer_card, 0, True)
-            else:
-                state = (player_score, dealer_card, -1, True)
         self.isterminal = state[3]
+        return state
+
+    def _step_player(self):
+        card, color = np.random.choice(self.cards), np.random.choice(self.colors, p=self.colors_prob)
+        self.player_cards.append((card, color))
+        player_score = self.calc_score(self.player_cards)
+        state = (player_score, self.dealer_card, 0, False) if 1 < player_score < 21 else (player_score, self.dealer_card, -1, True)
+        return state
+
+    def _step_dealer(self):
+        # dealer card drawing rule
+        while 0 < self.calc_score(self.dealer_cards) < 17:
+            card, color = np.random.choice(self.cards), np.random.choice(self.colors, p=self.colors_prob)
+            self.dealer_cards.append((card, color))
+
+        player_score, dealer_score = self.calc_score(self.player_cards), self.calc_score(self.dealer_cards)
+        if dealer_score > 21 or player_score > dealer_score:
+            state = (player_score, self.dealer_card, 1, True)
+        elif player_score == dealer_score:
+            state = (player_score, self.dealer_card, 0, True)
+        else:
+            state = (player_score, self.dealer_card, -1, True)
         return state
 
 
 def main():
     np.random.seed(14)
     env = Easy21()
-    env.start_episode()
+    env.restart_episode()
 
     print(env.player_cards, env.dealer_cards)
 
-    state = env.step("HIT")
-    while state[3] is False:
+    while env.isterminal is False:
         state = env.step("HIT")
         print(env.player_cards, env.dealer_cards)
 
